d68f9c2bbcf696ef7f031fe0a037e379906ed730
Jacob <techsupport@kreindler.ca>
2022-09-23 09:36:47 -0400

    feat: updated status

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import interactions
 from interactions import Client, Intents
 import os
 from dotenv import load_dotenv
-import asyncio
-import random
 
 load_dotenv()
-token = os.getenv('TOKEN')
 
-bot = Client(token=os.getenv('TOKEN'), presence=interactions.ClientPresence(activities=[interactions.PresenceActivity(name="Drawing Tutorials", type=interactions.PresenceActivityType.WATCHING)]), intents=Intents.GUILD_MESSAGE_CONTENT)
-
-guild_ids = [918591198799749240]
+bot = Client(token=os.getenv('TOKEN'), presence=interactions.ClientPresence(activities=[interactions.PresenceActivity(name="Safety Third", type=interactions.PresenceActivityType.LISTENING)]), intents=Intents.GUILD_MESSAGE_CONTENT)
 
 @bot.event
 async def on_ready():
