8ff65e26c28fab02e646eca703003c331d2a0047
Jacob <techsupport@kreindler.ca>
2023-09-07 22:29:00 -0400

    fixed status

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import interactions
 from interactions import listen
-from interactions.api.events import Startup
+from interactions.api.events import Startup, GuildJoin
 import os
 from dotenv import load_dotenv
 
+from interactions import listen
+
+
 load_dotenv()
 
-bot = interactions.Client(token=os.getenv(
-    'TOKEN'), default_scope=918591198799749240)
+bot = interactions.Client(token=os.getenv('TOKEN'), activity=interactions.Activity.create(
+    name="Safety Third", type=interactions.ActivityType.LISTENING), intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MEMBERS, default_scope=918591198799749240)
 
 
 @listen(Startup)
 async def on_startup():
     print('Ready!')
     print('------')
+
+
+@listen(GuildJoin)
+async def on_guild_join(event: GuildJoin):
+    print(f"Guild joined : {event.guild.name}")
 
 
 for filename in os.listdir('./commands'):
