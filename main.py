6034e01fa653b961acf6f367e80944d59034f2cf
Jacob <techsupport@kreindler.ca>
2023-11-10 21:41:53 -0500

    added fun little thing for anyone who uses a nono word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import interactions
 from interactions import listen
-from interactions.api.events import Startup, GuildJoin
+from interactions.api.events import Startup, GuildJoin, MessageCreate
+from better_profanity import profanity
 import os
 from dotenv import load_dotenv
 
@@ -10,7 +11,7 @@ from interactions import listen
 load_dotenv()
 
 bot = interactions.Client(token=os.getenv('TOKEN'), activity=interactions.Activity.create(
-    name="why dogs > cats", type=interactions.ActivityType.LISTENING), intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MEMBERS, default_scope=918591198799749240)
+    name="why dogs > cats", type=interactions.ActivityType.LISTENING), intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MEMBERS | interactions.Intents.MESSAGE_CONTENT, default_scope=918591198799749240)
 
 
 @listen(Startup)
@@ -24,10 +25,18 @@ async def on_guild_join(event: GuildJoin):
     print(f"Guild joined : {event.guild.name}")
 
 
+@listen(MessageCreate)
+async def on_message(event: MessageCreate):
+    messageContent = event.message.content.lower()
+    if event.message.author.id == 963255439963869244:
+        return
+    elif profanity.contains_profanity(messageContent):
+        await event.message.reply("https://tenor.com/view/captain-america-marvel-avengers-gif-14328153")
+        await event.message.author.send(f"Hi {event.message.author.mention}! Just sending this about {event.message.jump_url}. We try and keep the robotics server server pretty clean, epecially since Mr. Wong doesn't like it when we swear so if you could do your best to keep it clean that would be great! Thanks!", surpressEmbeds=True)
+
 for filename in os.listdir('./commands'):
     if filename.endswith('.py'):
         bot.load_extension(f'commands.{filename[:-3]}')
         print(f'Loaded: {filename[:-3]}')
-
 
 bot.start()
